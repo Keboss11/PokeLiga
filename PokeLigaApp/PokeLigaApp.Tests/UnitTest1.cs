230e411a691c2efa03f8968145b4eaf47365d976
Ronny Ancorini <8093082+RonnyAnc@users.noreply.github.com>
2024-12-17 20:03:28 +0100

    test: first test

---
+++ PokeLigaApp/PokeLigaApp.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace PokeLigaApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    private int Sum(int x, int y)
+    {
+        return x + y;
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var x = 1;
+        var y = 2;
+        
+        var result = Sum(x, y);
+        
+        Assert.That(result, Is.EqualTo(3));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var x = 2;
+        var y = 2;
+        
+        var result = Sum(x, y);
+        
+        Assert.That(result, Is.EqualTo(4));
+    }
+}
